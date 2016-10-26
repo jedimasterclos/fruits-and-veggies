8945e048f011e93e31f3726592ba44d0b2359a4e
jedimasterclos <carlossrinivas@gmail.com>
2016-10-26 10:52:08 -0700

    Carlos: Fruits&Veg

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
-/* setup your angular app here */
+var app = angular.module('fruitVeg', []);
 
-// debug stuff to show the app is loading and fruit / veggies are available
+app.controller('mainCtrl', ['$scope', function($scope) {
+  $scope.fruits = []
+  $scope.veggies = []
+  
+ 	$scope.randomize = function(array) {
+		var result = [];
+
+		for (var i = 0; i < array.length; i++) {
+			var index = Math.floor(Math.random() * (array.length) + 1);
+			result.push(array[index]);
+		}
+		return result;
+	}
+	
+	$scope.foods = $scope.randomize(fruits.concat(vegetables));
+
+  $scope.moveLeft = function(idx) {
+    $scope.fruits.push($scope.foods[idx]);
+    $scope.foods.splice(idx, 1);
+  }
+
+  $scope.moveRight = function(idx) {
+    $scope.veggies.push($scope.foods[idx]);
+    $scope.foods.splice(idx, 1);
+  }
+
+  $scope.moveBackLeft = function(idx) {
+    $scope.foods.push($scope.veggies[idx]);
+    $scope.veggies.splice(idx, 1);
+  }
+
+  $scope.moveBackRight = function(idx) {
+    $scope.foods.push($scope.fruits[idx]);
+    $scope.fruits.splice(idx, 1);
+  };
+}]);
+
 console.log('App Started');
 console.log('Fruit count', fruits.length);
 console.log('Veggie count', vegetables.length);
+
